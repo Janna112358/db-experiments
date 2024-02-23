a034411ccbb1b0e34c64e3c29437541644b83adb
Janna Goldstein <janna@impactinstitute.com>
2024-02-23 14:46:40 +0100

    add project for tryNpgsql
    getting somewhere with a raw query for the columns

--- README.md
+++ README.md
@@ -13,12 +13,14 @@ psql -d postgres -U [username] -f sql/create_db.sql
 Set an environment variable named "CONNSTRING_GAMES_LOCAL".
 
 
-## SQLHydra
+## TryHydra
 
-We use SqlHydra to automatically generate a "database types" module `DbTypes.fs` (in the repo). Regenerate the hydra file by running: 
+Project to try out (experimental release of) sqlhydra.
+
+We use SqlHydra to automatically generate a "database types" module `tryHydra/DbTypes.fs` (in the repo). Regenerate the hydra file by running: 
 
 ```shell
-dotnet sqlhydra npgsql --project db-experiments.fsproj
+dotnet sqlhydra npgsql --project tryHydra/tryHydra.fsproj
 ```
 
 The first time you do this, you will be promted for some input parameters. These parameters are then saved in a file called `sqlhydra-npgsql.toml` so you don't have to give the input every time. If you want to change the params, such as which database is being used, you can edit the `.toml` file directly.
@@ -27,7 +29,11 @@ Choose/fill in the following parameters when prompted:
 
 ```shell
 > Enter a database Connection String: Server=localhost;Database=games;Username=<username>;Password=<password>
-> Enter an Output Filename (...): DbTypes.fs
+> Enter an Output Filename (...): tryHydra/DbTypes.fs
 > Enter a Namespace (...): Games.DbTypes
 > Select a use case: Sqlhydra.Query integration (default)
 ```
+
+## TryNpgsql
+
+Trying to query the metadata on Postgres materialized views with Npgsql (used in the hydra code). 
---
+++ tryNpgsql/Program.fs
@@ -0,0 +1,117 @@
+﻿let getConnectionString () =
+    match System.Environment.GetEnvironmentVariable "CONNSTRING_GAMES_LOCAL" with
+    | null -> failwith "Could not find environment variable CONNSTRING_GAMES_LOCAL"
+    | con -> con
+
+let getMViews (conn: Npgsql.NpgsqlConnection) =
+    // below is from the hydra code for regular views
+    //     let sViews = conn.GetSchema("Views")
+    //
+    //     let views =
+    //         sViews.Rows
+    //         |> Seq.cast<System.Data.DataRow>
+    //         |> Seq.map (fun tbl ->
+    //             {| Catalog = tbl["TABLE_CATALOG"] :?> string
+    //                Schema = tbl["TABLE_SCHEMA"] :?> string
+    //                Name = tbl["TABLE_NAME"] :?> string
+    //                Type = "view" |})
+
+    let sMaterializedViews = conn.GetSchema("MaterializedViews")
+
+    let materializedViews =
+        sMaterializedViews.Rows
+        |> Seq.cast<System.Data.DataRow>
+        |> Seq.map (fun tbl ->
+            {| Catalog = tbl["TABLE_CATALOG"] :?> string
+               Schema = tbl["TABLE_SCHEMA"] :?> string
+               Name = tbl["TABLE_NAME"] :?> string
+               Type = "view" |})
+
+    materializedViews
+
+let getColumns (conn: Npgsql.NpgsqlConnection) =
+    let sColumns = conn.GetSchema("Columns")
+
+    sColumns.Rows
+    |> Seq.cast<System.Data.DataRow>
+    |> Seq.map (fun col ->
+        {| TableCatalog = col["TABLE_CATALOG"] :?> string
+           TableSchema = col["TABLE_SCHEMA"] :?> string
+           TableName = col["TABLE_NAME"] :?> string
+           ColumnName = col["COLUMN_NAME"] :?> string
+           ProviderTypeName = col["DATA_TYPE"] :?> string
+           OrdinalPosition = col["ORDINAL_POSITION"] :?> int
+           IsNullable =
+            match col["IS_NULLABLE"] :?> string with
+            | "YES" -> true
+            | _ -> false |})
+    |> Seq.sortBy (fun column -> column.OrdinalPosition)
+
+let getColumns2 (conn: Npgsql.NpgsqlConnection) =
+    let sqlQuery =
+        """
+        SELECT 
+            pg_namespace.nspname AS table_schema, 
+            pg_class.relname AS table_name, 
+            pg_attribute.attname AS column_name, 
+            pg_attribute.attnum AS ordinal_position
+        FROM pg_catalog.pg_class
+        INNER JOIN pg_catalog.pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+        INNER JOIN pg_catalog.pg_attribute ON pg_class.oid = pg_attribute.attrelid
+        WHERE 
+            pg_attribute.attnum >= 1
+        ORDER BY 
+            table_schema, 
+            table_name, 
+            column_name
+        """
+
+    use cmd = new Npgsql.NpgsqlCommand(sqlQuery, conn)
+    use rdr = cmd.ExecuteReader()
+
+    [ while rdr.Read() do
+          {| TableCatalog = "" //col["TABLE_CATALOG"] :?> string
+             TableSchema = rdr.["TABLE_SCHEMA"] :?> string
+             TableName = rdr.["TABLE_NAME"] :?> string
+             ColumnName = rdr.["COLUMN_NAME"] :?> string
+             ProviderTypeName = "" // col["DATA_TYPE"] :?> string
+             OrdinalPosition = "" // col["ORDINAL_POSITION"] :?> int
+          // IsNullable =
+          //  match col["IS_NULLABLE"] :?> string with
+          //  | "YES" -> true
+          //  | _ -> false
+          |} ]
+    |> Set.ofList
+
+[<EntryPoint>]
+let main args =
+    let connString = getConnectionString ()
+    use conn = new Npgsql.NpgsqlConnection(connString)
+    conn.Open()
+
+    let mViewInfo = getMViews conn |> Seq.head
+    printf $"first materialized view: {mViewInfo}\n"
+
+    let mViewColumns =
+        getColumns conn
+        |> Seq.filter (fun c ->
+            c.TableCatalog = mViewInfo.Catalog
+            && c.TableSchema = mViewInfo.Schema
+            && c.TableName = mViewInfo.Name)
+
+    // nothing cause the materialized view columns aren't stored in the Information schema
+    // which is apparently where GetSchema("Columns") gets its data from
+    printfn "Attempt 1: materialized view columns?"
+    mViewColumns |> Seq.iter (printfn "%A")
+
+    let mViewColumns2 =
+        getColumns2 conn
+        |> Seq.filter (fun c ->
+            // c.TableCatalog = mViewInfo.Catalog &&
+            c.TableSchema = mViewInfo.Schema && c.TableName = mViewInfo.Name)
+
+    printfn "Attempt 2: materialized view columns?"
+    mViewColumns2 |> Seq.iter (printfn "%A")
+
+
+    0
